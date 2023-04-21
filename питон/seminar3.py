454b6d1afc88c6be140583b781024f59f9f47036
marysh001 <mariashlinchak@yandex.ru>
2023-04-21 10:36:29 +0300

    ggg

---
+++ питон/seminar3.py
@@ -0,0 +1,33 @@
+#Задача 16
+n = (input())
+d = {}
+d = dict(array n)
+d ['x'] = imax
+print (x)
+sum dictionary['x']
+for item in dictionary:
+    print('{}:{}' .sum(item, dictionary[item]))
+
+#сортируем все остальные числа в массиве которые не x 
+def quick_sort(array n):
+if len(array n) <= 1:
+    return array n
+else:
+    x = (imax)
+    less = [i for i in array[1:] if i< x]
+    greater = [i for i in array[1:] if i > x]
+    return quick_sort (less) + [x] + quick_sort(greater)
+    print (quick_sort(x))
+
+#Задача 18: 
+def quick_sort(array):
+if len(array) <= 1:
+    return array n
+else:
+    x = ()
+    less = [i for i in array[1:] if i<= x]
+    greater = [i for i in array[1:] if i >= x]
+    return quick_sort (less) + [x] + quick_sort(greater)
+    print (quick_sort(x))
+    if i in array = (x+=1) or i in array = (x-=1)
+print(i)
