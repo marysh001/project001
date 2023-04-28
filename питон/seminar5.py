0980f8955866a6e15012a83c1979f349a8bb3721
marysh001 <mariashlinchak@yandex.ru>
2023-04-28 09:16:13 +0300

    a

---
+++ питон/seminar5.py
@@ -0,0 +1,22 @@
+#Задача 26: 
+print ('введите чисто a')
+a = (int input())
+print ('введите чисто b')
+b = (int input())
+c = 0
+if a <= -1 or b <= -1:
+print ('введите целое не отриц число')
+c = (a**b)
+print(c)
+
+#Задача 28:
+def sum_numbers(a,b)
+print ('введите чисто a')
+a = (int input())
+print ('введите чисто b')
+b = (int input())
+summa = 0
+summa = (a + b)
+if a > 0 or a < 0  and b > 0 or b < 0:
+return summa
+
