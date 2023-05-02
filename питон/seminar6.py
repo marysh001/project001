92b200babc54af43a0bfa421770cf2a2151078d3
marysh001 <mariashlinchak@yandex.ru>
2023-05-02 09:18:55 +0300

    s

---
+++ питон/seminar6.py
@@ -0,0 +1,18 @@
+#Задача 30: 
+def progress(a1,n,d)
+print ('введите число a1')
+a1 = (int input())
+print ('введите число n')
+n = (int input())
+print ('введите число d')
+d = (int input())
+for i in range (n):
+     print (a1+ i*d)
+
+#Задача 32:
+list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+min_number = int(input())
+max_number = int(input())
+for i in range(len(list_1)):
+if min_number <= list_1[i] <= max_number:
+print(i)
