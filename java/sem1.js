f1012ccfebbda42d19c43c7b39c438bc7df917ff
marysh001 <mariashlinchak@yandex.ru>
2023-06-12 19:51:35 +0300

    r

---
+++ java/sem1.js
@@ -0,0 +1,39 @@
+public class Program {
+    public static void main(String[] args)
+    { 
+        /*1 пункт */
+        int [] ints = new int[1];
+        Random random = new Random();
+        for (int j = 0; j < ints.length; j ++)
+        {
+        ints[i] = random.nextInt(2000);
+        System.out.println(i)
+
+}
+    
+/*клон массива +10элем расшир память*/
+
+int[] tmp = new int[ints.length +10];
+for (int j = 0; j < ints.length; j ++) 
+    tmp[j] = ints[j];
+    ints = tmp;
+}
+/*2 пункт*/
+n = int highestOneBit(int i)
+System.out.println(n)
+
+/*3 пункт, пытаюсь прописать что остаток = 0*/
+int [m] ints = new int[10];
+        for (int j = 0; j < ints.length; j ++)
+        { if (i/n) && float(i) = 0;
+System.out.println( i .toUpperCase().repeat().length(Short.MAX_VALUE))
+
+        }
+        /*4 пункт*/
+        int [m2] ints = new int[10];
+        for (int j = 0; j < ints.length; j ++)
+        { if (i/n) && float(i) != 0;
+System.out.println( i .toLowerCase().repeat().length(Short.MIN_VALUE))
+ 
+}
+}
