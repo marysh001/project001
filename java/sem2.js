042cd39491693fc97aa40276835ea2701e14e612
marysh001 <mariashlinchak@yandex.ru>
2023-06-16 12:28:22 +0300

    builder

---
+++ java/sem2.js
@@ -0,0 +1,28 @@
+public class Program {
+    public static void main(String[] args)
+    { 
+
+        String str1 = "name";
+        String str2 = "country"
+        String str3 = "city"
+        String str4 = "age"
+
+str.getBytes();
+System.out.println(str.toUpperCase());
+if(str.isEmpty());
+if (str.length()==0);
+
+System.out.println(str.charAt(str.length()/2));
+System.out.println(str.indexOf("!"));
+
+builder = new StringBuilder(32);
+StringBuilder builder1 = new StringBuilder("Ivanov");
+StringBuilder builder2 = new StringBuilder("Russia");
+StringBuilder builder3 = new StringBuilder("Moscow");
+StringBuilder builder4 = new StringBuilder("34");
+
+
+str = builder.toString();
+System.out.prntln(builder1, builder2, builder3, builder4);
+
+    }}
