89cfbe4c6d03c36f496beda7fe55903e34362c13
marysh001 <mariashlinchak@yandex.ru>
2023-07-04 12:46:18 +0300

    всякое

---
+++ js begin/java/sem3.js
@@ -0,0 +1,27 @@
+public class Program {
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> list = new ArrayList<>();
+     for (int i = 0; i < 100; i++)
+
+     {list.add(new Random().nextInt(100))}
+     for (int j != 0; j < Array.length; j ++);
+     {if (i/2) && (float(i) = 0);
+     list.remove(i);
+     }
+
+     list.sort (i .toUpperCase().repeat().length(Short.MAX_VALUE))
+     System.out.println(i .toUpperCase().repeat().length(Short.MAX_VALUE))
+
+    
+     list.sort (i .toLowerCase().repeat().length(Short.MIN_VALUE))
+     System.out.println(i .toLowerCase().repeat().length(Short.MIN_VALUE))
+
+     sum = 0;
+     for (int j != 0; j < Array.length; j ++) {
+        sum += i;
+        }
+            System.out.println("среднее арифметическое чисел равно: " + sum/list.length);
+    }
+        
+    }
