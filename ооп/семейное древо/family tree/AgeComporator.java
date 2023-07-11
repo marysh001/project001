86c9e1946f1c80097cee106d1a980bf055b98e2a
me <mariashlinchak@yandex.ru>
2023-07-11 16:52:32 +0300

    /

--- ооп/семейное древо/family tree/AgeComporator.java
+++
@@ -1,9 +0,0 @@
-package family_tree;
-import java.util.Comparator;
-
-public class AgeComparator implements Comparator<Human>{
-    @Override
-    public int compare(Human o1,Human o2 ){
-        return Integer.compare(o1.age, o2.age);
-    }
-}
--- ооп/семейное древо/family tree/GenderComporator.java
+++
@@ -1,9 +0,0 @@
-package family_tree;
-import java.util.Comparator;
-
-public class GenderComparator implements Comparator<Human>{
-    @Override
-    public String compare(Human o1,Human o2 ){
-        return String.compare(o1.gender, o2.gender);
-    }
-}
